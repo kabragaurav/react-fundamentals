99955c9dd84a421403f3d348cda5e3b3a6b0bbf1
gkabra <gkabra@salesforce.com>
2024-05-14 17:01:12 +0530

    All edits

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -9,44 +9,36 @@ function App() {
    * Default code from CRA
    */
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-
-
-  /**
-   * Code to test custom component
-   */
-
-  // return (
-  //   <div className="App">
-  //     <MyCustomComp/>
-  //   </div>
-  // );
-
-  /**
-   * Code to render a custom Amazon product tile
-   */
   return (
+    <>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+
+    <div className="App">
+      <MyCustomComp/>
+    </div>
+
     <div className="App">
       <Product/>
     </div>
+    </>
+    
   );
+
 }
 
 export default App;
