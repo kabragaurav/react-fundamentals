3802160191c50326a1672e33d1f951bfdf2f9fdb
gkabra <gkabra@salesforce.com>
2024-05-15 16:08:26 +0530

    axios

---
+++ react-fundamentals/src/services/ProductService.js
@@ -0,0 +1,6 @@
+import Axios from 'axios';
+
+export const getProducts = () => {
+    const url = 'https://raw.githubusercontent.com/mdmoin7/Random-Products-Json-Generator/master/products.json';
+    return Axios.get(url);
+}
